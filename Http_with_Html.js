706c5076ee9356d6b92663ef8378f937a45e2055
Engr-Md-Bahauddin <bahauddin33-3041@diu.edu.bd>
2026-05-26 18:49:27 +0600

    fix render deploy2

--- Http_with_Html.js
+++ Http_with_Html.js
@@ -1,41 +1,38 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const PORT = process.env.PORT || 3000;
-// const hostname = '192.168.0.170';
+function fileHandel(fileName, statusCode, res) {
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.write("<h1>File Not Found</h1>");
+            return res.end();
+        }
 
-const server = http.createServer((req,res) => {
- 
-    
-const fileHandel = (filelocation, statuscode) => {
-        fs.readFile(filelocation, (err,data) =>{
-        res.writeHead(statuscode, {'Content-Type': 'text/html'});
+        res.writeHead(statusCode, { "Content-Type": "text/html" });
         res.write(data);
         res.end();
-        });
-
-    };
-
-if(req.url === '/'){
-    fileHandel('./HTML_Files/index.html', 200);
+    });
 }
 
-else if(req.url === '/about'){
-    fileHandel('./HTML_Files/about.html', 200);
-}
+const server = http.createServer((req, res) => {
 
-else if(req.url === '/contact'){
-    fileHandel('./HTML_Files/contact.html', 200);
-}
-
-else{
-    fileHandel('./HTML_Files/error.html', 404);
-}
-
-
+    if (req.url === "/") {
+        fileHandel("index.html", 200, res);
+    }
+    else if (req.url === "/about") {
+        fileHandel("about.html", 200, res);
+    }
+    else if (req.url === "/contact") {
+        fileHandel("contact.html", 200, res);
+    }
+    else {
+        fileHandel("error.html", 404, res);
+    }
 });
 
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log(`Server Running on Port ${PORT}`);
+    console.log("Server Running on Port " + PORT);
 });
