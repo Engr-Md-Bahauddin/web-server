07e7d1c79556a19ce3c4e8de58ed02b7427a51cf
Engr-Md-Bahauddin <bahauddin33-3041@diu.edu.bd>
2026-05-26 20:06:58 +0600

    big change

--- Http_with_Html.js
+++ Http_with_Html.js
@@ -1,38 +1,36 @@
 const http = require("http");
 const fs = require("fs");
+const PORT = process.env.PORT || 3000;
 
-function fileHandel(fileName, statusCode, res) {
+
+
+const server = http.createServer((req, res) => {
+
+function fileHandel(fileName, statusCode) {
     fs.readFile(fileName, (err, data) => {
-        if (err) {
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.write("<h1>File Not Found</h1>");
-            return res.end();
-        }
-
+        if(err) {console.log(err);}
         res.writeHead(statusCode, { "Content-Type": "text/html" });
         res.write(data);
         res.end();
     });
-}
-
-const server = http.createServer((req, res) => {
+ }
 
     if (req.url === "/") {
-        fileHandel("./HTML_Files/Index.html", 200, res);
+        fileHandel("./HTML_Files/Index.html", 200);
     }
     else if (req.url === "/about") {
-        fileHandel("./HTML_Files/about.html", 200, res);  // ✅ FIXED
+        fileHandel("./HTML_Files/about.html", 200); 
     }
     else if (req.url === "/contact") {
-        fileHandel("./HTML_Files/contact.html", 200, res);
+        fileHandel("./HTML_Files/contact.html", 200);
     }
     else {
-        fileHandel("./HTML_Files/error.html", 404, res);
+        fileHandel("./HTML_Files/error.html", 404,);
     }
 });
 
-const PORT = process.env.PORT || 3000;
+
 
 server.listen(PORT, () => {
-    console.log("Server Running on Port " + PORT);
+    console.log(`Server Running on Port : ${PORT}`);
 });
